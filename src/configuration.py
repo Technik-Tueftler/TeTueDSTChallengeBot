63df81601e706fbb066f1170a3c51bd5e6debff7
Technik Tüftler <technik.tueftler@gmail.com>
2025-03-10 22:34:43 +0100

    refactor generic configuration

--- src/configuration.py
+++ src/configuration.py
@@ -6,8 +6,8 @@ from dotenv import load_dotenv
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from .db import DbConfiguration
-from .tetue_generic.generic_requests import GenReqConfiguration, init_generic_requests
-from .tetue_generic.watcher import WatcherConfiguration, init_generic_watcher
+from .tetue_generic.generic_requests import GenReqConfiguration
+from .tetue_generic.watcher import WatcherConfiguration
 
 load_dotenv("default.env")
 load_dotenv("files/.env", override=True)
@@ -21,19 +21,10 @@ class Configuration(BaseSettings):
         BaseSettings (BaseSettings): Base class for settings from environment variables.
     """
 
-    model_config = SettingsConfigDict(env_prefix="TT_", env_nested_delimiter="__", arbitrary_types_allowed=True)
+    model_config = SettingsConfigDict(
+        env_prefix="TT_", env_nested_delimiter="__", arbitrary_types_allowed=True
+    )
 
     gen_req: GenReqConfiguration
     watcher: WatcherConfiguration
     db: DbConfiguration
-
-
-def set_configurations(configuration: Configuration) -> None:
-    """
-    Initialization of the generic functions
-
-    Args:
-        configuration (Configuration): App configuration with validated values
-    """
-    init_generic_requests(configuration.gen_req.request_timeout)
-    init_generic_watcher(configuration.watcher.log_file_path)

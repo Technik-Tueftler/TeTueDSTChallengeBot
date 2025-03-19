f9683d4640ccf197a7c5f210a73e44859672ba3d
Technik Tüftler <technik.tueftler@gmail.com>
2025-03-20 00:31:29 +0100

    change own implementation of UserSelect to generic UserSelect that a search function is available

--- src/discord_bot.py
+++ src/discord_bot.py
@@ -6,7 +6,7 @@ The bot is implemented using the discord.py library and provides a simple comman
 from pydantic import BaseModel
 import discord
 from discord.ext import commands
-from discord import SelectOption, Interaction
+from discord import Interaction
 
 
 class DiscordBotConfiguration(BaseModel):
@@ -22,24 +22,23 @@ intents.members = True  # Option necessary to get members in guild
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 
-class UserSelect(discord.ui.Select):
+class TextInput(discord.ui.TextInput):
+    ...
+
+class UserSelect(discord.ui.UserSelect):
     """
     UserSelect class to create a user selection menu to select up to 6 users for the game.
     """
 
-    def __init__(self, users):
-        options = [
-            SelectOption(label=user.name, value=str(user.id)) for user in users[:6]
-        ]
+    def __init__(self):
         super().__init__(
             placeholder="Select up to 6 user for the game",
             min_values=1,
             max_values=6,
-            options=options,
         )
 
     async def callback(self, interaction: Interaction):
-        selected = [f"<@{user_id}>" for user_id in self.values]
+        selected = [user.mention for user in self.values]
         await interaction.response.send_message(
             f"The players for game \"Fast and hungry, task hunt\" are: : {', '.join(selected)}",
             # ephemeral=True Option that all user can see the message
@@ -51,9 +50,9 @@ class UserSelectView(discord.ui.View):
     UserSelectView class to create a view for the user selection menu with handler.
     """
 
-    def __init__(self, users):
+    def __init__(self):
         super().__init__(timeout=60)
-        self.add_item(UserSelect(users))
+        self.add_item(UserSelect())
         self.message = None
 
     async def on_timeout(self):
@@ -86,10 +85,12 @@ async def game1(interaction: discord.Interaction):
     player has completed all tasks.
     """
     if interaction.guild:
-        view = UserSelectView(interaction.guild.members)
+        view = UserSelectView()
         await interaction.response.send_message(
             "Select the players for the game \"Fast and hungry, task hunt\":", 
             view=view,
             ephemeral=True
         )
+
         view.message = await interaction.original_response()
+        

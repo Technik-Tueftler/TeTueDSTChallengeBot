fb96ee5e900ce91f4abcf378ea741918bdea32ad
Technik Tüftler <technik.tueftler@gmail.com>
2025-03-21 11:32:46 +0100

    intermediate score with chaining for text input and transfer of players

--- src/discord_bot.py
+++ src/discord_bot.py
@@ -7,6 +7,22 @@ from pydantic import BaseModel
 import discord
 from discord.ext import commands
 from discord import Interaction
+from typing import List
+
+
+class Player:
+    """
+    Player class to store the player name and the playing hours.
+    """
+
+    def __init__(self, name: str, dc_number: str, hours: str):
+        self.name = name
+        self.dc_id = dc_number
+        self.hours = hours
+
+
+    def __str__(self):
+        return f"Player {self.name} with id: {self.dc_id} has played {self.hours} hours."
 
 
 class DiscordBotConfiguration(BaseModel):
@@ -22,27 +38,33 @@ intents.members = True  # Option necessary to get members in guild
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 
-class TextInput(discord.ui.TextInput):
-    ...
-
-class UserSelect(discord.ui.UserSelect):
+class PlayerLevelInput(
+    discord.ui.Modal, title="Enter the playing hours for each player"
+):
     """
-    UserSelect class to create a user selection menu to select up to 6 users for the game.
+    PlayerLevelInput class to create a input menu with for the player levels.
     """
 
-    def __init__(self):
-        super().__init__(
-            placeholder="Select up to 6 user for the game",
-            min_values=1,
-            max_values=6,
-        )
+    def __init__(self, player_list: List[Player]):
+        super().__init__()
+        self.player_list = player_list
+        for player in player_list:
+            self.add_item(
+                discord.ui.TextInput(
+                    label=player.name,
+                    placeholder=f"Enter the playing hours for player {player.name}",
+                    required=True,
+                    max_length=5,
+                )
+            )
 
-    async def callback(self, interaction: Interaction):
-        selected = [user.mention for user in self.values]
-        await interaction.response.send_message(
-            f"The players for game \"Fast and hungry, task hunt\" are: : {', '.join(selected)}",
-            # ephemeral=True Option that all user can see the message
-        )
+    async def on_submit(self, interaction: discord.Interaction):
+        mapping = {child.label: child.value for child in self.children}
+        for player in self.player_list:
+            player.hours = mapping[player.name]
+        # await interaction.response.send_message(f"Player 1: {self.hours_1.value}")
+        await interaction.response.send_message("Fertig")
+        self.stop()
 
 
 class UserSelectView(discord.ui.View):
@@ -50,18 +72,25 @@ class UserSelectView(discord.ui.View):
     UserSelectView class to create a view for the user selection menu with handler.
     """
 
-    def __init__(self):
-        super().__init__(timeout=60)
-        self.add_item(UserSelect())
-        self.message = None
+    def __init__(self, player_list: List):
+        super().__init__()
+        self.player_list = player_list
 
-    async def on_timeout(self):
-        for item in self.children:
-            item.disabled = True
-        if self.message:
-            await self.message.edit(
-                content="Time has expired and is deactivated.", view=self
-            )
+    @discord.ui.select(
+        cls=discord.ui.UserSelect,
+        placeholder="Select up to 6 user for the game",
+        min_values=1,
+        max_values=6,
+    )
+    async def user_select(
+        self, interaction: Interaction, select: discord.ui.UserSelect
+    ):
+        #print(select.values)
+        #selected = [user.mention for user in select.values]
+        self.player_list = [Player(user.name, user.id, 0) for user in select.values]
+        await interaction.response.send_modal(PlayerLevelInput(self.player_list))
+        # await interaction.response.send_message(f"The players for game \"Fast and hungry, task hunt\" are: : {', '.join(selected)}")
+        self.stop()
 
 
 @bot.event
@@ -70,27 +99,60 @@ async def on_ready():
     Event function to print a message when the bot is online.
     """
     print(f"{bot.user} ist online")
-    await bot.tree.sync()
+    try:
+        synced = await bot.tree.sync()
+        print(f"Slash Commands synchronisiert: {len(synced)}")
+    except Exception as e:
+        print(e)
 
 
 @bot.tree.command(
     name="fast_and_hungry_task_hunt",
-    description="Complete all tasks and survive. The game ends as " \
+    description="Complete all tasks and survive. The game ends as "
     "soon as one player has completed all tasks",
 )
 async def game1(interaction: discord.Interaction):
     """
     Command function to start a game with a user selection menu. This game is
-    about completing all tasks and surviving. The game ends as soon as one 
+    about completing all tasks and surviving. The game ends as soon as one
     player has completed all tasks.
     """
     if interaction.guild:
-        view = UserSelectView()
-        await interaction.response.send_message(
-            "Select the players for the game \"Fast and hungry, task hunt\":", 
-            view=view,
-            ephemeral=True
-        )
+        player_list = []
+        try:
+            user_view = UserSelectView(player_list)
+            await interaction.response.send_message(
+                'Select the players for the game "Fast and hungry, task hunt":',
+                view=user_view,
+                ephemeral=True,
+            )
+            await user_view.wait()
+            print("beendet")
+            for player in player_list:
+                print(player)
+            print(user_view.player_list)
+            # print(user_view.children[0].values)
+            selected = [user.mention for user in user_view.children[0].values]
+        except Exception as e:
+            print(e)
 
-        view.message = await interaction.original_response()
-        
+        # try:
+        # await interaction.response.send_message(f"The players for game \"Fast and hungry, task hunt\" are: : {', '.join(selected)}")
+        # except Exception as e:
+        # print(e)
+        # print((f"The players for game \"Fast and hungry, task hunt\" are: : {', '.join(selected)}"))
+
+
+async def main():
+    import os
+    from dotenv import load_dotenv
+
+    load_dotenv("files/.env", override=True)
+    TOKEN = os.getenv("TT_DC__token")
+    await bot.start(TOKEN)
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(main())
